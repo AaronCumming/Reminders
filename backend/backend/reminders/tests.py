34573f1b8b386357f4df6efc9ed4d650dcec4580
Aaron Cumming <aaroncumming04@gmail.com>
2025-04-24 13:56:53 -0500

    tests implented

--- backend/backend/reminders/tests.py
+++ backend/backend/reminders/tests.py
@@ -1,3 +1,33 @@
+from django.core.exceptions import ValidationError
 from django.test import TestCase
+from django.utils import timezone 
+
+from reminders.models import Event, Reminder
+
 
 # Create your tests here.
+
+class TestEventModel(TestCase):
+    def test_name_validation(self):
+        name = "i" * 257
+        event_time =  timezone.now()
+        event = Event(name=name, event_time=event_time)
+
+        with self.assertRaises(ValidationError):
+            event.full_clean()
+
+    def test_str(self):
+        event = Event(name="My Event", event_time =  timezone.now())
+
+        result = str(event)
+
+        self.assertEqual(result, "My Event")
+
+
+class TestReminderModel(TestCase):
+    def test_reminder_time(self):
+        time = timezone.now()
+        event = Event(name="My Event", event_time =  timezone.now())
+        reminder = Reminder(event = event, remind_time = time)
+
+        self.assertEqual(reminder.remind_time, time)
